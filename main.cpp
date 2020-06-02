9cdb5e2f606b876e04284685545f52137035dddb
jdeoey49 <60366471+jdeoey49@users.noreply.github.com>
2020-06-02 15:43:30 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,37 +1,22 @@
-// main.cpp
-// Testing class BasePlusCommissionEmployee.
-#include <iostream>
-#include <iomanip>
-#include <stdexcept>
-#include "BasePlusCommissionEmployee.h" // class definition
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include"CommissionEmployee.h"
 using namespace std;
-
 int main()
 {
-   try {
-	   // try to instantiate BasePlusCommissionEmployee object
-   		BasePlusCommissionEmployee 
-      	employee( "Bob", "Lewis", "333-33-3333", 5000, .04, 300 );
-   
-   		// set floating-point output formatting
-   		cout << fixed << setprecision( 2 );
+	//create base-class object
+	cout << fixed << setprecision(2);
+	CommissionEmployee commissionEmployee("Sue", "Jones", "222-22-2222", 10000, .06);
+	cout << "Print base-class object:\n\n";
+	commissionEmployee.print();
+	cout << endl << endl;
 
-		employee.print();
-   		employee.setBaseSalary( 1000 ); // set base salary
-		employee.setCommissionRate(0.1);
-
-   		cout << "\nUpdated employee information output by print function: \n" 
-      		 << endl;
-   		employee.print(); // display the new employee information
-   
-   		// display the employee's earnings
-   		cout << "\n\nEmployee's earnings: $" << employee.earnings() << endl;
-   	} // end try
-	
-	catch (invalid_argument& e ){
-		cerr << "\nInvalid argument: " << e.what() << endl;
-	} // end catch
-} // end main
-
-
-
+	//create base-class pointer and aim it at base-class object
+	CommissionEmployee* commissionemployeePtr = &commissionEmployee;
+	(*commissionemployeePtr).setGrossSales(1000000);
+	commissionemployeePtr->setCommissionRate(0.07);
+	commissionemployeePtr->print();//invokes base-class print
+	cout << endl;
+	return 0;
+}
