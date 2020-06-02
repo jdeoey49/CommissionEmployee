590ecabf8d47a4515e990a48c05116b7bb89cc65
jdeoey49 <60366471+jdeoey49@users.noreply.github.com>
2020-06-02 16:20:00 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,29 +8,16 @@ using namespace std;
 
 int main()
 {
-   try {
-	   // try to instantiate BasePlusCommissionEmployee object
-   		BasePlusCommissionEmployee 
-      	employee( "Bob", "Lewis", "333-33-3333", 5000, .04, 300 );
-   
-   		// set floating-point output formatting
-   		cout << fixed << setprecision( 2 );
+	BasePlusCommissionEmployee basePlusComissionEmploee("Bob", "Lewis", "333-33-3333", 5000, .04, 300);
 
-		employee.print();
-   		employee.setBaseSalary( 1000 ); // set base salary
-		employee.setCommissionRate(0.1);
+	BasePlusCommissionEmployee* basePlusCommissionEmploeePtr = &basePlusComissionEmploee;
 
-   		cout << "\nUpdated employee information output by print function: \n" 
-      		 << endl;
-   		employee.print(); // display the new employee information
-   
-   		// display the employee's earnings
-   		cout << "\n\nEmployee's earnings: $" << employee.earnings() << endl;
-   	} // end try
-	
-	catch (invalid_argument& e ){
-		cerr << "\nInvalid argument: " << e.what() << endl;
-	} // end catch
+	cout << "Print derived-class object:\n\n";
+	basePlusComissionEmploee.print();//invoked derived-class print
+	cout << endl << endl;
+	(*basePlusCommissionEmploeePtr).setBaseSalary(1000);
+	basePlusCommissionEmploeePtr->print();//invokes derived-class print
+	cout << endl;
 } // end main
 
 
