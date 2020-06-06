477929a23b663aaac1806bd3d6b816a130cde8e6
jdeoey49 <60366471+jdeoey49@users.noreply.github.com>
2020-06-06 13:35:25 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,29 +8,49 @@ using namespace std;
 
 int main()
 {
-   try {
-	   // try to instantiate BasePlusCommissionEmployee object
-   		BasePlusCommissionEmployee 
-      	employee( "Bob", "Lewis", "333-33-3333", 5000, .04, 300 );
-   
-   		// set floating-point output formatting
-   		cout << fixed << setprecision( 2 );
+	try
+	{
+		//base-class pointer to base-class object(invoked base-class member function)
+		/*
+		CommissionEmployee commissionEmployee("Sue", "Jones", "222-22-2222", 10000,.06);
+		cout << "Print base-class object:\n\n";
+		commissionEmployee.print();//invoked base-class print
+		cout << endl << endl;
 
-		employee.print();
-   		employee.setBaseSalary( 1000 ); // set base salary
-		employee.setCommissionRate(0.1);
+		CommissionEmployee* commissionEmploeePtr = &commissionEmployee;
+		cout << "Print base-class object\n\n";
+		commissionEmploeePtr->print();//invoke base-class print
+		cout << endl;
+		*/
 
-   		cout << "\nUpdated employee information output by print function: \n" 
-      		 << endl;
-   		employee.print(); // display the new employee information
-   
-   		// display the employee's earnings
-   		cout << "\n\nEmployee's earnings: $" << employee.earnings() << endl;
-   	} // end try
+		//derived-class pointer to derived-class object(invoked derive-class member function)
+		/*
+		BasePlusCommissionEmployee basePlusComissionEmploee("Bob", "Lewis", "333-33-3333", 5000, .04, 300);
+		cout << "Print derived-class object:\n\n";
+		basePlusComissionEmploee.print();//invoked derived-class print
+		cout << endl << endl;
+
+		BasePlusCommissionEmployee* basePlusCommissionEmploeePtr = &basePlusComissionEmploee;
+		(*basePlusCommissionEmploeePtr).setBaseSalary(1000);
+		basePlusCommissionEmploeePtr->print();//invokes derived-class print
+		cout << endl;
+		*/
+
+		//base-class pointer to derived-class object(invoked base-class member function)
+		BasePlusCommissionEmployee basePlusComissionEmploee("Bob", "Lewis", "333-33-3333", 5000, .04, 300);
+
+
+		CommissionEmployee* CommissionEmployeePtr = &basePlusComissionEmploee;
+		cout << "\nCalling print with base-class pointer to "
+			<< "derived-class object\ninvokes base-class print "
+			<< "function on that derived-class object/n/n";
+		CommissionEmployeePtr->print();//invokes base-class print
+	}
+	catch(invalid_argument& i)
+	{
+		cout << i.what();
+	}
 	
-	catch (invalid_argument& e ){
-		cerr << "\nInvalid argument: " << e.what() << endl;
-	} // end catch
 } // end main
 
 
